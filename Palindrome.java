be5943f9fc78d50ded2a1bcbc63f32f8414aff51
mehdimouden <>
2021-04-08 15:35:54 +0000

    Voici le code

--- Palindrome.java
+++ Palindrome.java
@@ -1,7 +1,17 @@
 public class Palindrome{
     public static boolean is_palindrome(String s)
     {
+      boolean is_palindrome = false;
+      for (int i=0;i<=s.length()-1;i++){
+        if(s.charAt(i)==s.charAt(s.length()-1-i)){
+          return true;
+        }
+      }
         // add your code here
-        return true;
+        return false;
+
     }
-}
+    
+
+
+    }
